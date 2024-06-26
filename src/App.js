c024adc7c89d4a3f109c0edbd86ed10beea12fa6
Gustavo Ribeiro <gustavo_ribeiro77@hotmail.com>
2024-06-26 04:34:41 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,36 @@
 import { useEffect, useState } from "react";
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
 
 function App() {
   const [users, setUsers] = useState([]);
@@ -20,7 +52,7 @@ function App() {
       ) : (
         <>
           <h1>Usuários</h1>
-          <table border={1}>
+          {/* <table border={1}>
             <tr>
               <th>Nome</th>
               <th>Data de Nascimento</th>
@@ -37,7 +69,33 @@ function App() {
                 <td>{user.status}</td>
               </tr>
             ))}
-          </table>
+          </table> */}
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 700 }} aria-label="customized table">
+              <TableHead>
+                <TableRow>
+                  <StyledTableCell>Nome</StyledTableCell>
+                  <StyledTableCell align="right">Data de Nascimento</StyledTableCell>
+                  <StyledTableCell align="right">CPF</StyledTableCell>
+                  <StyledTableCell align="right">Endereço</StyledTableCell>
+                  <StyledTableCell align="right">Status</StyledTableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {users.map((row) => (
+                  <StyledTableRow key={row.name}>
+                    <StyledTableCell component="th" scope="row">
+                      {row.name}
+                    </StyledTableCell>
+                    <StyledTableCell align="right">{row.birth_date}</StyledTableCell>
+                    <StyledTableCell align="right">{row.cpf}</StyledTableCell>
+                    <StyledTableCell align="right">{row.address}</StyledTableCell>
+                    <StyledTableCell align="right">{row.address}</StyledTableCell>
+                  </StyledTableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
         </>
       )}
     </div>
